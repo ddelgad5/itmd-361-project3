3e20e1f04452f37aca83701c04ddcd3d465c9470
David Delgado <ddelgad5@hawk.iit.edu>
2017-12-07 22:31:34 -0600

    Added javascript for Spotify API.

--- site.js
+++ site.js
@@ -1,27 +1,44 @@
 // Avoid conflicting variables
 $.noConflict();
 
+var yourtoken = "";
 (function($) {
 
-  //Fetch album songs
+
+  //Fetch Spotify Token
+  $(document).ready(function() {
+    $(location).attr('href');
+    yourtoken = window.location.hash.substr(1);
+    console.log("This is your token:",yourtoken);
+  });
+
+  //Verify Token and Fetch Tracklist
   $('.album').click(function(event) {
-    console.log("Clicked on an album.  The album is id'd as:", $(this).attr('id'));
-    var mytoken = "BQAHKAIeAVXqXxQOEZYsOjnP5p7o2lQ85U6GrwxzW-S6Us9CAxCWYlIyUfBER4l61JAgHDN_AyhRhU8eZtzlLo4sCYCa1KviYY1WFr2DBbYgBBNuP-xYQiV9EHU-z5d1qgHKaXiQ2bfu5ZY";
-    var albumid = $(this).attr('id');
-    var url1 = "https://api.spotify.com/v1/albums/"+albumid+"/tracks";
-    $.ajax({
-         url: url1,
-         data: { "signature": "authHeader" },
-         type: "GET",
-         headers: {
-           "Accept": "application/json",
-           "Content-Type": "application/json",
-           "Authorization": "Bearer "+mytoken,
-         },
-         success: function(data) {
-           console.log(data);
-         }
+    if (yourtoken == ""){
+      console.log("yourtoken is null");
+      document.location.href= "https://accounts.spotify.com/authorize?client_id=2712479b9c5f4b4da5ba43507064c8eb&redirect_uri=http://daviddelgado.co/music&response_type=token";
+    }
+    else {
+      console.log("Clicked on an album.  The album is id'd as:", $(this).attr('id'));
+      var albumid = $(this).attr('id');
+      var url1 = "https://api.spotify.com/v1/albums/"+albumid+"/tracks";
+      $.ajax({
+        url: url1,
+        data: { "signature": "authHeader" },
+        type: "GET",
+        headers: {
+         "Accept": "application/json",
+         "Content-Type": "application/json",
+         "Authorization": "Bearer "+yourtoken
+        },
+        success: function(data) {
+         console.log(data);
+        }
       });
+    }
   });
 
 })(jQuery);
+
+// Implicit Grant Flow URL
+// https://accounts.spotify.com/authorize?client_id=2712479b9c5f4b4da5ba43507064c8eb&redirect_uri=http://daviddelgado.co/music&response_type=token
