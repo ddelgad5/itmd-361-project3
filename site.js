12a7b6c75d919fa7884894d2abc2e7e84e9a8738
David Delgado <ddelgad5@hawk.iit.edu>
2017-12-07 19:59:05 -0600

    Added ajax get for albums.

--- site.js
+++ site.js
@@ -2,7 +2,26 @@
 $.noConflict();
 
 (function($) {
-  $('.album').on('click', function (event) {
-    console.log("Clicked on an album.  The album is id'd as: ", event.target.id);
+
+  //Fetch album songs
+  $('.album').click(function(event) {
+    console.log("Clicked on an album.  The album is id'd as:", $(this).attr('id'));
+    var mytoken = "BQAHKAIeAVXqXxQOEZYsOjnP5p7o2lQ85U6GrwxzW-S6Us9CAxCWYlIyUfBER4l61JAgHDN_AyhRhU8eZtzlLo4sCYCa1KviYY1WFr2DBbYgBBNuP-xYQiV9EHU-z5d1qgHKaXiQ2bfu5ZY";
+    var albumid = $(this).attr('id');
+    var url1 = "https://api.spotify.com/v1/albums/"+albumid+"/tracks";
+    $.ajax({
+         url: url1,
+         data: { "signature": "authHeader" },
+         type: "GET",
+         headers: {
+           "Accept": "application/json",
+           "Content-Type": "application/json",
+           "Authorization": "Bearer "+mytoken,
+         },
+         success: function(data) {
+           console.log(data);
+         }
+      });
   });
+
 })(jQuery);
