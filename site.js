816ee3b0a49642b0643254f525177031f2c7a1bc
David Delgado <ddelgad5@hawk.iit.edu>
2017-12-07 22:44:37 -0600

    Modified spacing and variable declarations.

--- site.js
+++ site.js
@@ -1,38 +1,40 @@
 // Avoid conflicting variables
 $.noConflict();
 
-var yourtoken = "";
 (function($) {
 
+  var yourtoken = "";
+  var albumid;
+  var url1;
 
-  //Fetch Spotify Token
+  // Fetch Spotify Token
   $(document).ready(function() {
     $(location).attr('href');
     yourtoken = window.location.hash.substr(1);
-    console.log("This is your token:",yourtoken);
+    console.log("This is your token:", yourtoken);
   });
 
-  //Verify Token and Fetch Tracklist
-  $('.album').click(function(event) {
-    if (yourtoken == ""){
+  // Verify Token and Fetch Tracklist
+  $('.album').click(function() {
+    if (yourtoken === ""){
       console.log("yourtoken is null");
       document.location.href= "https://accounts.spotify.com/authorize?client_id=2712479b9c5f4b4da5ba43507064c8eb&redirect_uri=http://daviddelgado.co/music&response_type=token";
     }
     else {
       console.log("Clicked on an album.  The album is id'd as:", $(this).attr('data-id'));
-      var albumid = $(this).attr('data-id');
-      var url1 = "https://api.spotify.com/v1/albums/"+albumid+"/tracks";
+      albumid = $(this).attr('data-id');
+      url1 = "https://api.spotify.com/v1/albums/"+albumid+"/tracks";
       $.ajax({
         url: url1,
         data: { "signature": "authHeader" },
         type: "GET",
         headers: {
-         "Accept": "application/json",
-         "Content-Type": "application/json",
-         "Authorization": "Bearer "+yourtoken
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": "Bearer "+yourtoken
         },
         success: function(data) {
-         console.log(data);
+          console.log(data);
         }
       });
     }
