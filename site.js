0a9e342d77e2834b088fc5b2743772248fa4abec
David Delgado <ddelgad5@hawk.iit.edu>
2017-12-07 22:52:05 -0600

    Modified var yourtoken and hash element.

--- site.js
+++ site.js
@@ -3,22 +3,22 @@ $.noConflict();
 
 (function($) {
 
-  var yourtoken = "";
+  var yourtoken;
   var albumid;
   var url1;
 
   // Fetch Spotify Token
   $(document).ready(function() {
     $(location).attr('href');
-    yourtoken = window.location.hash.substr(1);
+    yourtoken = window.location.hash.access_token;
     console.log("This is your token:", yourtoken);
   });
 
   // Verify Token and Fetch Tracklist
   $('.album').click(function() {
-    if (yourtoken === ""){
+    if (yourtoken === undefined){
       console.log("yourtoken is null");
-      document.location.href= "https://accounts.spotify.com/authorize?client_id=2712479b9c5f4b4da5ba43507064c8eb&redirect_uri=http://daviddelgado.co/music&response_type=token";
+      document.location.href= "https://accounts.spotify.com/authorize?client_id=2712479b9c5f4b4da5ba43507064c8eb&redirect_uri=http://daviddelgado.co/music/&response_type=token";
     }
     else {
       console.log("Clicked on an album.  The album is id'd as:", $(this).attr('data-id'));
